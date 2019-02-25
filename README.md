dbee1b9f82af1b71022f3332189228139e17cba1
Cole Gannon <Arcanehood@gmail.com>
2019-02-24 23:15:55 -0800

    A lot of YAML

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# eslint-config
+# @coalpha/eslint-config
+
+### An eslint config for people who think they know what they're doing
+
 My personal eslint config for personal files
+Since this config extends eslint:recommended, there's no need to include some things.
+There's a lot of dependence on what eslint decides is default.
+At some point I may need to update this if eslint changes defaults.
+If you think this looks a lot like airbnb's eslint config, you'd be right. I like their config a lot.
+
+## warn
+
+- Saves time
+- Has a legitimate use
+- Might look better visually
+
+## error
+
+- Gross
+- Never any use
+- No reason it should happen
---
+++ build.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const { load } = require("js-yaml");
+
+const resolveYAML = location => load(fs.readFileSync(location, "utf8"), { noCompatMode: true });
+
+const base = resolveYAML("./.eslintrc.yml");
+
+function minify(str) {
+  switch (`${str}`) {
+    case "false":
+      return 0
+    case "true":
+    case "warn":
+      return 1
+    case "error":
+      return 2
+    default:
+      return str;
+  }
+}
+base.rules = Object.assign(
+  Object.create(null),
+  ...["best-practices", "errors", "node", "style", "variables"]
+  .map(fileName => {
+    const rules = resolveYAML(`./rules/${fileName}.yml`);
+    Object.keys(rules).forEach(key => {
+      const val = rules[key];
+      rules[key] = Array.isArray(val) ? val.map(minify) : minify(val);
+    });
+    return rules;
+  })
+);
+fs.writeFileSync(
+  path.join(__dirname, "index.js"),
+  `module.exports=${JSON.stringify(base).replace(/"(\w+)":/g, "$1:")}`
+);
+
