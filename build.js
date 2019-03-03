7f13cc0d8d361fdfe0b1c0516ece8eee48864749
Cole Gannon <Arcanehood@gmail.com>
2019-03-03 04:40:13 -0800

    Okay, another patch

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 const File = require("./File");
 
-const base = new File("base.yml").asYAML();
-
-const rulesArray = File.readDir("rules").map(file => file.asYAML().obj);
+const base = new File("base.yml");
+base.asYAML();
+const rulesArray = File.readDir("rules").map((file) => { file.asYAML(); file.obj; });
 base.obj.rules = Object.assign({}, ...rulesArray);
 base.encodeAsJS();
 base.fileName = "index.js";
